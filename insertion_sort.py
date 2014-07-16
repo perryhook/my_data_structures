4d1cdf5cf36b173542c40e60f197a60e12b05aae
Perry Hook <perry.hook@gmail.com>
2014-07-16 09:31:15 -0700

    basic graphing capabilities

--- insertion_sort.py
+++ insertion_sort.py
@@ -15,7 +15,9 @@ def sort(list):
 
 if __name__ == '__main__':
     import timeit
-    repetitions = 100
+    import matplotlib.pyplot as plt
+
+    repetitions = 10
 
     long_random_list_test = """
         l = [random.randint(-99999,99999) for i in xrange(0, 1000)]
@@ -29,28 +31,45 @@ if __name__ == '__main__':
     print "1000 item random int list with {} repetions takes time {}".format(
         repetitions, long_random_time)
 
-    long_des_case_test = """
-        l = [i for i in xrange(1000, 0, -1)]
+    descend_case = """
+        l = [i for i in xrange({}, 0, -1)]
         insertion_sort.sort(l)
         """
-    long_des_case_time = timeit.timeit(
-        long_des_case_test,
-        setup="import insertion_sort",
-        number=repetitions
-    )
-    print \
-        "1000 item decending int list with {} repetions takes time {}".format(
-            repetitions, long_des_case_time)
 
-    long_asc_case_test = """
-        l = [i for i in xrange(0, 1000)]
+    ascend_case = """
+        l = [i for i in xrange(0, {})]
         insertion_sort.sort(l)
         """
-    long_asc_case_time = timeit.timeit(
-        long_asc_case_test,
-        setup="import insertion_sort",
-        number=repetitions
-    )
-    print \
-        "1000 item ascending int list with {} repetions takes time {}".format(
-            repetitions, long_asc_case_time)
+
+    plt.hold(True)
+    descend_results = []
+    for size in xrange(50, 2501, 50):
+        time = timeit.timeit(
+            descend_case.format(size),
+            setup="import insertion_sort",
+            number=repetitions
+        )
+        descend_results.append((size, time))
+        # print "result: ({}, {})".format(size, time)
+
+    ascend_results = []
+    for size in xrange(50, 2501, 50):
+        time = timeit.timeit(
+            ascend_case.format(size),
+            setup="import insertion_sort",
+            number=repetitions
+        )
+        ascend_results.append((size, time))
+        # print "result({}, {})".format(size, time)
+
+#    print descend_results
+#    print ascend_results
+
+    for i in range(len(descend_results)):
+        n_descend = descend_results[i][0]
+        time_descend = descend_results[i][1]
+        n_ascend = ascend_results[i][0]
+        time_ascend = ascend_results[i][1]
+        plt.plot(n_descend, time_descend, 'bo')
+        plt.plot(n_ascend, time_ascend, 'ro')
+    plt.show()
