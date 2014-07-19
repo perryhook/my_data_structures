3c11550bec268f0d07a2701f631e3e2a84be1028
Perry Hook <perry.hook@gmail.com>
2014-07-19 14:13:04 -0700

    "added timing and plotting code to main"

--- mergesort.py
+++ mergesort.py
@@ -43,3 +43,46 @@ def merge(a, lo, hi):
     # copy tmp back into a
     for j in xrange(lo, hi+1):
         a[j] = tmp[j-lo]
+
+
+if __name__ == '__main__':
+    import timeit
+    repetitions = 100
+
+    long_random_list_test = """
+        l = [random.randint(-99999,99999) for i in xrange(0, 1000)]
+        merge_sort.sort(l)
+        """
+    long_random_time = timeit.timeit(
+        long_random_list_test,
+        setup="import merge_sort, random",
+        number=repetitions
+    )
+    print "1000 item random int list with {} repetions takes time {}".format(
+        repetitions, long_random_time)
+
+    long_des_case_test = """
+        l = [i for i in xrange(1000, 0, -1)]
+        merge_sort.sort(l)
+        """
+    long_des_case_time = timeit.timeit(
+        long_des_case_test,
+        setup="import merge_sort",
+        number=repetitions
+    )
+    print \
+        "1000 item decending int list with {} repetions takes time {}".format(
+            repetitions, long_des_case_time)
+
+    long_asc_case_test = """
+        l = [i for i in xrange(0, 1000)]
+        merge_sort.sort(l)
+        """
+    long_asc_case_time = timeit.timeit(
+        long_asc_case_test,
+        setup="import merge_sort",
+        number=repetitions
+    )
+    print \
+        "1000 item ascending int list with {} repetions takes time {}".format(
+            repetitions, long_asc_case_time)
